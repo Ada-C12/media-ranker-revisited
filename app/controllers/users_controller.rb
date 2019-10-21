6b1fd6fe0066b2d2662594f3d6eee53c68498757
Eve Le <nguyenthao.lee@gmail.com>
2019-10-21 15:53:47 -0700

    Update logout action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,20 +13,29 @@ class UsersController < ApplicationController
     
     user = User.find_by(uid: auth_hash[:uid], provider: "github")
     if user
-      flash[:success] = "Logged in as returning user #{user.name}"
+      session[:user_id] = user.id
+      flash[:status] = :success
+      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
     else
       user = User.build_from_github(auth_hash)
-      
       if user.save
-        flash[:success] = "Logged in as new user #{user.name}"
+        session[:user_id] = user.id
+        flash[:status] = :success
+        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
       else
-        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        flash.now[:status] = :failure
+        flash.now[:result_text] = "Could not log in"
+        flash.now[:messages] = user.errors.messages
         return redirect_to root_path
       end
     end
-
-    session[:user_id] = user.id
     return redirect_to root_path
   end
   
+  def destroy
+    session[:user_id] = nil
+    flash[:status] = :success
+    flash[:result_text] = "Successfully logged out"
+    redirect_to root_path
+  end
 end
