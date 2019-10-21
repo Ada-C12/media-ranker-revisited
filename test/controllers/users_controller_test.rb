53943555aa343572679c72c5b6e6e33fed5a34c2
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-21 16:47:33 -0700

    Added first test for Users controller for logging in a returning user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,21 @@
 require "test_helper"
 
 describe UsersController do
+  describe "auth_callback" do
+    it "logs in an existing user and redirects to the root route" do
+      start_count = User.count
+
+      user = users(:dan)
+      perform_login(user)
+
+      must_redirect_to root_path
+
+      session[:user_id].must_equal user.id
+
+      User.count.must_equal start_count
+    end
+
+    it "creates an account for a new user and redirects to the root route" do
+    end
+  end
 end
