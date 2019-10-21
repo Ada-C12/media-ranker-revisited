c915d4e04a8b67eb356e45705850e47674b4c782
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-21 14:36:27 -0700

    added route to login with github

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
-
+  
   resources :works
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
-
+  
   resources :users, only: [:index, :show]
+  
+  get "/auth/github", as: "github_login"
 end
