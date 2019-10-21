1638c20b49bd545519b8ec37455c23868ea1fb8e
Eve Le <nguyenthao.lee@gmail.com>
2019-10-21 16:23:09 -0700

    Update tests to use valid users data with valid uid and provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+  validates :uid, presence: true
+  validates :provider, presence: true
 
   def self.build_from_github(auth_hash)
     user = User.new
