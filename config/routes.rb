b92a4afadeb76a6ecb109f8432224c82189f2a08
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-21 15:45:45 -0700

    Cleaning up work.

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
   root "works#root"
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "users#create"
-  # get "/login", to: "users#login_form", as: "login"
-  # post "/login", to: "users#login"
-  # post "/logout", to: "users#logout", as: "logout"
+  delete "/logout", to: "users#destroy", as: "logout"
 
   resources :works
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
   resources :users, only: [:index, :show]
-
-  delete "/logout", to: "users#destroy", as: "logout"
 end
