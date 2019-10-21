77caf302fd54517de1949aed157652332bd3daee
Julia Bouvier <juliabouv@gmail.com>
2019-10-21 15:15:19 -0700

    Implemented OAuth with Github

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,25 +2,23 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
+  
   def show
     @user = User.find_by(id: params[:id])
     render_404 unless @user
   end
-
-  def login_form
-  end
-
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
+  
+  def create
+    auth_hash = request.env["omniauth.auth"]
+    
+    user = User.find_by(uid: auth_hash[:uid], provider: "github")
+    if user
       flash[:status] = :success
       flash[:result_text] = "Successfully logged in as existing user #{user.username}"
     else
-      user = User.new(username: username)
+      user = User.build_from_github(auth_hash)
+      
       if user.save
-        session[:user_id] = user.id
         flash[:status] = :success
         flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
       else
@@ -31,13 +29,16 @@ class UsersController < ApplicationController
         return
       end
     end
-    redirect_to root_path
+    
+    session[:user_id] = user.id
+    return redirect_to root_path
   end
 
   def logout
     session[:user_id] = nil
     flash[:status] = :success
     flash[:result_text] = "Successfully logged out"
+
     redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
-
+  
   validates :username, uniqueness: true, presence: true
+  
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash[:uid]
+    user.provider = "github"
+    user.name = auth_hash["info"]["name"]
+    user.email = auth_hash["info"]["email"]
+    user.username = auth_hash["extra"]["raw_info"]["login"]
+    
+    return user
+  end
 end
