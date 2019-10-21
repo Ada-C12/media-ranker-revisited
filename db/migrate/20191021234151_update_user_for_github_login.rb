40704d1a8d0f6b6f8320537775bf4b905da666a6
geli-gel <angele.zam@gmail.com>
2019-10-21 16:44:00 -0700

    added columns to User for github login data

---
+++ db/migrate/20191021234151_update_user_for_github_login.rb
@@ -0,0 +1,8 @@
+class UpdateUserForGithubLogin < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :uid, :integer
+    add_column :users, :provider, :string
+    add_column :users, :name, :string
+    add_column :users, :email, :string
+  end
+end
