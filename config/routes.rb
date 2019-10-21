79f2c597ca368be659a76f3d36c59ed3f8663c00
geli-gel <angele.zam@gmail.com>
2019-10-21 16:21:02 -0700

    removed old login routes and added github auth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   root "works#root"
 
   get "/auth/github", as: "github_login"
-
-  get "/login", to: "users#login_form", as: "login"
-  post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
+
+  get "/auth/:provider/callback", to: "users#create"
 
   resources :works
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
