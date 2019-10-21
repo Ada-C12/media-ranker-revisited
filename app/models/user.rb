b2dd4f34f0327deee72b97287fd289a0fe1b626c
Mira <calopter@gmail.com>
2019-10-21 16:32:44 -0700

    updates user model and db

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,16 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
-  validates :username, uniqueness: true, presence: true
+  validates :username, presence: true
+  validates :uid, presence: true
+  validates :provider, presence: true
+  
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash[:uid]
+    user.provider = "github"
+    user.username = auth_hash["info"]["name"]
+
+    return user
+  end
 end
