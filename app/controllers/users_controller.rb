4e7bef904d675f03ec32b6c210bb0e48a67cc70a
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-22 07:40:12 -0700

    fixed User find by in Users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
   def create
     auth_hash = request.env["omniauth.auth"]
     
-    user = User.find_by(id: auth_hash[:uid], provider: "github")
+    user = User.find_by(uid: auth_hash[:uid], provider: "github")
     
     if user
       flash[:success] = "Logged in as returning user #{user.name}"
