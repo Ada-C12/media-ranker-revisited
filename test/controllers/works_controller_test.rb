a1cc67067565833d1bcd14e741cae0ebc7f72a93
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-02 15:26:03 -0700

    Added works controller test for upvote: redirects to the work page if no user is logged in.

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -189,19 +189,21 @@ describe WorksController do
 
   describe "upvote" do
     it "redirects to the work page if no user is logged in" do
-      skip
+      post upvote_path(existing_work.id)
+      must_respond_with :redirect
+      must_redirect_to work_path
     end
 
     it "redirects to the work page after the user has logged out" do
-      skip
+      
     end
 
     it "succeeds for a logged-in user and a fresh user-vote pair" do
-      skip
+      
     end
 
     it "redirects to the work page if the user has already voted for that work" do
-      skip
+
     end
   end
 end
