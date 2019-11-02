906e911be6c1acf4f46b849ffa6d15d3c0a0e5e1
Hallie <idhallie@gmail.com>
2019-11-02 12:35:02 -0700

    Modified users model and pass tests for login.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_233500) do
+ActiveRecord::Schema.define(version: 2019_11_02_193009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", id: :serial, force: :cascade do |t|
-    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "uid"
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -16,7 +16,7 @@ describe UsersController do
     it "creates an account for a new user and redirects to the root route" do
       # new_user = users(:grace)
       # new_user.uid = 420
-      new_user = User.new(name:"Kathy", email: "whatev@git.com", uid: 473837 )
+      new_user = User.new(name:"Hallie", email: "hfake@fake.com", uid: 473837 )
       
       OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(new_user))
       expect{ get auth_callback_path(:github) }.must_change "User.count", 1
@@ -26,7 +26,7 @@ describe UsersController do
     end
     
     it "redirects to the login route if given invalid user data" do
-      new_user = User.new(name:"Kathy", email: "whatev@git.com", uid: nil )
+      new_user = User.new(name:"Hallie", email: "hfake@fake.com", uid: nil )
       
       OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(new_user))
       expect{ get auth_callback_path(:github) }.wont_change "User.count"
