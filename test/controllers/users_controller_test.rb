955447244c388a0279444dae6b93e2434a1060f9
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-02 14:05:53 -0700

    Started on users controller tests: added not logged in tests for index and show, all are passing.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,36 @@
 require "test_helper"
 
 describe UsersController do
+  
+  before do
+    @user = User.create(username: "testy_mctesterson", email: "test@test.com", provider: "Github", uid: 1234)
+  end
+  
+  describe "while not logged in" do
+    
+    describe "index" do
+      it "can get the index path" do
+        get users_path
+        
+        must_respond_with :success
+      end
+    end
+
+    describe "show" do
+      it "can get a valid user" do
+        get user_path(@user.id)
+        
+        must_respond_with :success
+      end
+      
+      it "will redirect for an invalid user" do
+        get user_path(-1)
+        
+        must_respond_with :not_found
+      end
+    end
+    
+  end
+  
 end
+
