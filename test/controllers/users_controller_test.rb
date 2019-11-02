5fdef28985c08c5b57b3c4c962d9258c612b9a8e
Hallie <idhallie@gmail.com>
2019-11-02 12:53:13 -0700

    Pass tests for logout.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 describe UsersController do
   
-  describe "auth_callback" do
+  describe "auth_callback (#create)" do
     it "logs in an existing user and redirects to the root route" do
       start_count = User.count
       
@@ -14,8 +14,6 @@ describe UsersController do
     end
     
     it "creates an account for a new user and redirects to the root route" do
-      # new_user = users(:grace)
-      # new_user.uid = 420
       new_user = User.new(name:"Hallie", email: "hfake@fake.com", uid: 473837 )
       
       OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(new_user))
@@ -33,5 +31,18 @@ describe UsersController do
       
       must_redirect_to root_path
     end
+  end
+  
+  describe "logout (#destroy)" do
+    before do
+      perform_login(users(:dan))
+    end
+    
+    it "logs out an already logged-in user" do
+      expect{ delete logout_path }.wont_change "User.count"
+      expect(session[:user_id]).must_equal nil
+      expect(flash[:success]).must_equal "Successfully logged out!"
+      must_redirect_to root_path
+    end 
   end
 end
