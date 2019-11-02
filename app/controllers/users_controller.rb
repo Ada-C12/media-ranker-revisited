af3570cae918b7cf4f22ffc5ac2c0bcf5179f0be
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-02 13:16:55 -0700

    Fiddled around with OAuth setup to try and make flash message display Github username/nickname. Decided to settle for displaying Github email address.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,50 @@
 class UsersController < ApplicationController
   
-  def create
-
-    auth_hash = request.env["omniauth.auth"]
-    
-    user = User.find_by(uid: auth_hash[:uid], provider: "github")
-    if user
-      # User was found in the database
-      flash[:success] = "Logged in as returning user #{user.name}"
+  def create    
+    if session[:user_id]
+      flash[:warning] = "A user is already logged in."
+      redirect_back(fallback_location: root_path)
     else
-      # User doesn't match anything in the DB
-      # Attempt to create a new user
-      user = User.build_from_github(auth_hash)
+      auth_hash = request.env["omniauth.auth"]
       
-      if user.save
-        flash[:success] = "Logged in as new user #{user.name}"
+      user = User.find_by(uid: auth_hash[:uid], provider: "github")
+      
+      if user
+        # User was found in the database
+        flash[:success] = "Logged in as returning user #{user.email}"
       else
-        # Couldn't save the user for some reason. If we
-        # hit this it probably means there's a bug with the
-        # way we've configured GitHub. Our strategy will
-       # be to display error messages to make future
-        # debugging easier.
-        flash[:danger] = "Could not create new user account: #{user.errors.messages}"
-        return redirect_to root_path
+        # User doesn't match anything in the DB
+        # Attempt to create a new user
+        user = User.build_from_github(auth_hash)
+        
+        if user.save
+          flash[:success] = "Logged in as new user #{user.email}"
+        else
+          # Couldn't save the user for some reason. If we
+          # hit this it probably means there's a bug with the
+          # way we've configured GitHub. Our strategy will
+          # be to display error messages to make future
+          # debugging easier.
+          flash[:danger] = "Could not create new user account: #{user.errors.messages}"
+          return redirect_to root_path
+        end
       end
+      
+      # If we get here, we have a valid user instance
+      session[:user_id] = user.id
+      return redirect_to root_path
     end
-    
-    # If we get here, we have a valid user instance
-    session[:user_id] = user.id
-    return redirect_to root_path
   end
   
   def destroy
-    session[:user_id] = nil
-    flash[:success] = "Successfully logged out!"
-    
-    redirect_to root_path
+    if session[:user_id] == nil
+      flash[:warning] = "You cannot log out because you are not logged in."
+    else
+      session[:user_id] = nil
+      flash[:success] = "Successfully logged out!"
+      
+      redirect_to root_path
+    end
   end
   
   def index
@@ -47,4 +56,10 @@ class UsersController < ApplicationController
     render_404 unless @user
   end
   
+end
+
+private
+
+def user_params
+  return params.require(:user).permit(:username, :email, :provider, :uid)
 end
