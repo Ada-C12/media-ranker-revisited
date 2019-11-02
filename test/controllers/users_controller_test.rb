e122b1005bb1f6c4d3a3788c06dc1b670b50ffe4
Hallie <idhallie@gmail.com>
2019-11-02 12:11:15 -0700

    Added SimpleCov to gemfile.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,37 @@
 require "test_helper"
 
 describe UsersController do
+  
+  describe "auth_callback" do
+    it "logs in an existing user and redirects to the root route" do
+      start_count = User.count
+      
+      perform_login
+      
+      must_redirect_to root_path
+      
+      expect(User.count).must_equal start_count
+    end
+    
+    it "creates an account for a new user and redirects to the root route" do
+      # new_user = users(:grace)
+      # new_user.uid = 420
+      new_user = User.new(name:"Kathy", email: "whatev@git.com", uid: 473837 )
+      
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(new_user))
+      expect{ get auth_callback_path(:github) }.must_change "User.count", 1
+      
+      must_redirect_to root_path
+      
+    end
+    
+    it "redirects to the login route if given invalid user data" do
+      new_user = User.new(name:"Kathy", email: "whatev@git.com", uid: nil )
+      
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(new_user))
+      expect{ get auth_callback_path(:github) }.wont_change "User.count"
+      
+      must_redirect_to root_path
+    end
+  end
 end
