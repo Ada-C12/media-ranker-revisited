e6a6e726ed8f9979447d89404d01c190c3289ce2
Hallie <idhallie@gmail.com>
2019-11-03 07:28:37 -0800

    Wave 2 complete. Passes all tests. 100% coverage.

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -191,18 +191,19 @@ describe WorksController do
   describe "upvote" do
     let(:work) { works(:album) }
     
-    
     it "redirects to the work page if no user is logged in" do
       post upvote_path(work.id)
+      flash[:result_text].must_equal "You must log in to do that"
       must_redirect_to work_path(work.id)
     end
     
-    it "redirects to the work page after the user has logged out" do
-      skip
-      # perform_login
-      # post upvote_path(@work.id)
-      # delete logout_path
-      # must_redirect_to work_path(@work.id)
+    it "redirects to the <strike>work page</strike> <add>root path</add> after the user has logged out" do
+      ## NOTE: the logout function is set to redirect to the root path
+      
+      perform_login
+      post upvote_path(work.id)
+      delete logout_path
+      must_redirect_to root_path
     end
     
     it "succeeds for a logged-in user and a fresh user-vote pair" do
@@ -220,7 +221,11 @@ describe WorksController do
     end
     
     it "redirects to the work page if the user has already voted for that work" do
+      perform_login(users(:kari))
+      post upvote_path(work.id)
       
+      flash[:result_text].must_equal "Could not upvote"
+      must_redirect_to work_path(work.id)
     end
   end
 end
