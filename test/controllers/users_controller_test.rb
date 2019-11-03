f400585f55ca604090ad23434b4b0f5c39808b66
Sam <samcoll@Samanthas-MBP.localdomain>
2019-11-03 14:50:30 -0800

    added login/logout tests using oauthmocks

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,58 @@
 require "test_helper"
 
 describe UsersController do
+  describe "auth_callback" do
+    it "logs in an existing user and redirects to root path" do
+      start_count = User.count
+      user = users(:kari)
+      perform_login(user)
+      
+      expect(session[:user_id]).must_equal user.id
+      expect(User.count).must_equal start_count
+      must_redirect_to root_path
+    end
+    
+    it "creates an account for a new user and redirects to the root route" do
+      start_count = User.count
+      user = User.new(provider: "github", uid: 99999, name: "test_user", email: "test@user.com")
+      
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+      get auth_callback_path(:github)
+      
+      expect(session[:user_id]).must_equal User.last.id
+      expect(User.count).must_equal start_count + 1
+      must_redirect_to root_path
+    end
+    
+    it "redirects to the login route if given invalid user data" do
+      start_count = User.count
+      user = User.new(provider: "github", uid: 99999, name: nil, email: "test@user.com")
+      
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+      get auth_callback_path(:github)
+      
+      expect(User.count).must_equal start_count
+      must_redirect_to root_path
+    end
+  end 
+  
+  describe "logged in users" do 
+    describe "destroy" do 
+      it "logs out a user" do 
+        start_count = User.count
+        user = users(:kari)
+        perform_login(user)
+        
+        expect(session[:user_id]).must_equal user.id
+        delete logout_path
+        
+        expect(session[:user_id]).must_be_nil
+        expect(User.count).must_equal start_count
+        must_redirect_to root_path
+      end
+    end
+  end 
+  
+  # describe "Guest users" do 
+  # end
 end
