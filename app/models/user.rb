2844f94389feaaf6d78a65b8fe8fa94081635eea
stupendousC <CarolineWuKaplan@gmail.com>
2019-11-02 22:22:45 -0700

    starting tests with 100% passing, haven't added any of my codes yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
   
+  validates :username, uniqueness: true, presence: true
+  
+  ### PRETTY SURE I'M SUPPOSED TO DO THIS TOO???
   # since there's only 1 OAuth Provider, don't have to worry about clashing uids from diff OAPs
-  validates :uid, presence: true
-  
-  
+  # validates :uid, presence: true
   
   def self.build_from_github(auth_hash)
     user = User.new
@@ -13,6 +14,9 @@ class User < ApplicationRecord
     user.provider = "github"
     user.name = auth_hash["info"]["name"]
     user.email = auth_hash["info"]["email"]
+    
+    # need this for the schema
+    user.username = auth_hash["info"]["name"]
     
     # Note that the user has not been saved.
     # We'll choose to do the saving outside of this method
@@ -22,5 +26,3 @@ class User < ApplicationRecord
 end
 
 
-# OLD validation
-# validates :username, uniqueness: true, presence: true
