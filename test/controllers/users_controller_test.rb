7494db4c71bca387abf637baa76610b833f1eee6
Mira <calopter@gmail.com>
2019-11-03 13:04:43 -0800

    stubs login tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,17 @@
 require "test_helper"
 
 describe UsersController do
+  describe 'auth_callback' do
+    it 'logs in an existing user and redirects to root' do
+      
+    end
+
+    it 'logs in a new user and redirects to root' do
+      
+    end
+
+    it 'redirects to root for invalid callbacks' do
+      
+    end
+  end
 end
