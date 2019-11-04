40785c64ed6c621f5f27593d971e09b3a58fb8e3
stupendousC <CarolineWuKaplan@gmail.com>
2019-11-03 18:00:22 -0800

    implemented require_login except before Works#root and Users#create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :find_user
-
+  
+  before_action :find_user, :require_login
+  
   def render_404
     # DPR: this will actually render a 404 page in production
     raise ActionController::RoutingError.new("Not Found")
   end
-
+  
+  def require_login
+    if @login_user.nil?
+      flash[:error] = "You must be logged in to view this section"
+      return redirect_to root_path
+    end
+  end
+  
   private
-
+  
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
