aac34bfe350c385b63796c0bcb704da22becdc71
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-11-03 17:53:04 -0800

    Complete #upvote tests. All pass.

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -189,19 +189,47 @@ describe WorksController do
 
   describe "upvote" do
     it "redirects to the work page if no user is logged in" do
-      skip
+      post upvote_path(existing_work.id)
+
+      expect(session[:user_uid]).must_be_nil
+      expect(flash[:result_text]).must_equal "You must log in to do that"
+      must_respond_with :redirect
+      must_redirect_to work_path(existing_work)
     end
 
     it "redirects to the work page after the user has logged out" do
-      skip
+      user = users(:dan)
+      perform_login(user)
+
+      work = works(:poodr)
+      delete logout_path(user)
+
+      must_respond_with :redirect
+      must_redirect_to root_path
     end
 
     it "succeeds for a logged-in user and a fresh user-vote pair" do
-      skip
+      user = users(:dan)
+      perform_login(user)
+
+      work = works(:poodr)
+
+     expect {
+        post upvote_path(work)
+        }.must_change "Vote.count", 1
+      expect(flash[:result_text]).must_equal "Successfully upvoted!"
+      must_redirect_to work_path(work)
     end
 
     it "redirects to the work page if the user has already voted for that work" do
-      skip
+      user = users(:dan)
+      perform_login(user)
+
+     expect {
+        post upvote_path(existing_work)
+        }.wont_change "Vote.count"
+      expect(flash[:result_text]).must_equal "Could not upvote"
+      must_redirect_to work_path(existing_work)
     end
   end
 end
