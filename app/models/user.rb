852c8a78e78e09c1856f90076368c0526ec09180
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-11-03 17:32:53 -0800

    gems added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
+  # It could be :uid
   validates :username, uniqueness: true, presence: true
 
   def self.build_from_github(auth_hash)
