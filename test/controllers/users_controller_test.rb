f6c0bd36e68da4ab4c1fefbf8b51a783e1f703b9
Mira <calopter@gmail.com>
2019-11-03 19:35:03 -0800

    login tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,11 +9,16 @@ describe UsersController do
     end
 
     it 'logs in a new user and redirects to root' do
-      
+      user = User.new(username: "test", uid: 1337, provider: "github")
+      expect { perform_login(user) }.must_change "User.count", 1
+      expect(session[:user_id]).must_equal User.last.id
+      must_redirect_to root_path
     end
 
     it 'redirects to root for invalid callbacks' do
-      
+      expect { perform_login(User.new) }.wont_change "User.count"
+      refute session[:user_id]
+      must_redirect_to root_path
     end
   end
 end
