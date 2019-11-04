131a99af41acb415246c2f083d777540d9540173
Natalie Tapias <nataliemtapias@gmail.com>
2019-11-04 09:04:01 -0800

    userscontroller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,8 @@
 require "test_helper"
 
 describe UsersController do
-let(:dan) { users(:dan) }
-let(:natalie ) {  }
-      
+  let(:dan) { users(:dan) }
+  let(:kari) { users(:kari) }
 
   describe 'auth_callback' do
     it "should log in an existing user" do
@@ -17,32 +16,32 @@ let(:natalie ) {  }
       
     it "should create a new user" do
       start_count = User.count
-      user_new = users(:kari)
-      user_new.uid = 2420
-      perform_login(user_new)
+      kari.uid = 2420
+      perform_login(kari)
     
       User.count.must_equal start_count + 1
+      session[:uid].must_equal kari.uid
+      must_redirect_to root_path
     end
 
     it "should flash error and redirect to root path if invalid user info provided" do
       start_count = User.count
       ted = User.new(provider: "github", name: "ted")
       perform_login(ted)
-      get auth_callback_path(:github)
-
       
       must_redirect_to root_path
       expect(flash[:error]).wont_be_nil
+      expect(flash[:success]).must_be_nil
       expect(flash[:uid]).must_be_nil
       expect(start_count).must_equal User.count
     end
+  end
 
     describe "logout" do
       it "should successfully log out a user" do
-        # log in 
-        OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(dan))
+        # log in
+        perform_login(dan)
 
-        get auth_callback_path(:github)
         must_redirect_to root_path
         session[:uid].must_equal dan.uid
 
@@ -51,14 +50,11 @@ let(:natalie ) {  }
         expect(session[:uid]).must_be_nil
         expect(flash[:success]).wont_be_nil
         must_redirect_to root_path
-
       end
     end
-  end
 
 
   describe 'not-authenticated' do
-
     describe 'index' do 
       it 'should show a list of valid users' do
         get users_path
