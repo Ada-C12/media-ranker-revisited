a38014eaae8a7317d89193b87f2d0f55d5a5279d
Emily Vomacka <emilyvomacka@gmail.com>
2019-11-04 11:36:43 -0800

    upvote tests passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ApplicationRecord
     user = User.new
     user.uid = auth_hash[:uid]
     user.provider = "github"
-    user.username = auth_hash["info"]["nickname"]
+    user.username = auth_hash["info"]["name"]
     user.email = auth_hash["info"]["email"]
+
     return user
   end
 end
