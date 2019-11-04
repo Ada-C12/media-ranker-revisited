0f53b8a729f3b524fac5d1adf98e3ce660b86be8
Dominique <dom_taylor@icloud.com>
2019-11-04 13:10:33 -0800

    wave 3 complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+  
 
   def render_404
     # DPR: this will actually render a 404 page in production
@@ -14,5 +15,13 @@ class ApplicationController < ActionController::Base
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
     end
+  end
+
+  def require_login
+    if find_user.nil?
+      flash[:status] = :failure
+      flash[:result_text] = "You must be logged in to view this section"
+      redirect_to root_path
+    end 
   end
 end
