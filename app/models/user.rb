9625393507991d312b228518ab52a4b0cf444821
daniela sanchez <dnsanche@gmail.com>
2019-11-04 13:22:18 -0800

    Flash stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,6 @@ class User < ApplicationRecord
     user.provider = "github"
     user.name = auth_hash["info"]["name"]
     user.email = auth_hash["info"]["email"]
-
-    # Note that the user has not been saved.
-    # We'll choose to do the saving outside of this method
     return user
   end
 end
