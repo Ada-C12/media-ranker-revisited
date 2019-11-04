c533494e3006f1307bc359db6bdba76e18976847
Dominique <dom_taylor@icloud.com>
2019-11-04 11:51:52 -0800

    Wave2 complete

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,78 @@
 require "test_helper"
+require "pry"
 
 describe UsersController do
+
+  # Tell OmniAuth to use mock data instead of connecting to Github
+  # Define a perform_login function for our tests that sends the login request using fixture data.
+  describe "auth_callback" do
+    # testing that this route is doing what it is suppose to do
+    it "logs in an existing user and redirects to the root route" do
+      # Count the users, to make sure we're not (for example) creating
+      # a new user every time we get a login request
+      start_count = User.count
+
+      # Get a user from the fixtures
+      user = users(:khabib)
+
+      # Tell OmniAuth to use this user's info when it sees
+      # an auth callback from github
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+      # Send a login request for that user
+      # Note that we're using the named path for the callback, as defined
+      # in the `as:` clause in `config/routes.rb`
+      get auth_callback_path(:github)
+
+      must_redirect_to root_path
+
+      # Since we can read the session, check that the user ID was set as expected
+      session[:user_id].must_equal user.id
+      
+
+      # Should *not* have created a new user
+      User.count.must_equal start_count
+    end
+
+    it "creates a new user and redirects" do
+      start_count = User.count
+      user = User.new(username: "taylor32", uid: 999, provider: "github", name: "Blaire", email: "taylor@gmail.com")
+
+      perform_login(user)
+      must_redirect_to root_path
+
+      # Should have created a new user
+      User.count.must_equal start_count + 1
+
+      # The new user's ID should be set in the session
+      session[:user_id].must_equal User.last.id
+    end 
+
+    it "redirects to the login route if given invalid user data" do
+      new_user = User.new(username: "taylor32", uid: nil, provider: "github", name: "Blaire", email: "taylor@gmail.com")
+      
+      perform_login(new_user)
+
+      expect{ get auth_callback_path(:github) }.wont_change "User.count"
+
+      must_redirect_to root_path
+    end 
+  end 
+
+  describe "logout" do
+    it "logs out a user and redirects" do
+      user = users(:khabib)
+  
+
+      perform_login(user)
+
+      delete logout_path
+
+      session[:user_id].must_equal nil
+
+      must_redirect_to root_path
+    end 
+
+    
+  end 
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 describe WorksController do
   let(:existing_work) { works(:album) }
+  
 
   describe "root" do
     it "succeeds with all media types" do
@@ -189,19 +190,51 @@ describe WorksController do
 
   describe "upvote" do
     it "redirects to the work page if no user is logged in" do
-      skip
+      
+      post upvote_path(existing_work.id)
+      
+      must_respond_with :redirect
+      must_redirect_to work_path(existing_work.id)
+      expect(flash[:status]).must_equal :failure
     end
 
     it "redirects to the work page after the user has logged out" do
-      skip
+
+      user = users(:khabib)
+
+      perform_login(user)
+
+      delete logout_path
+
+      post upvote_path(existing_work.id)
+      
+      must_respond_with :redirect
+      must_redirect_to work_path(existing_work.id)
+      expect(flash[:status]).must_equal :failure
     end
 
     it "succeeds for a logged-in user and a fresh user-vote pair" do
-      skip
+      user = users(:khabib)
+
+      perform_login(user)
+
+      work = works(:poodr)
+
+      post upvote_path(work.id)
+
+      expect(flash[:status]).must_equal :success
     end
 
     it "redirects to the work page if the user has already voted for that work" do
-      skip
+      user = users(:khabib)
+
+      perform_login(user)
+
+      post upvote_path(existing_work.id)
+
+      expect(flash[:result_text]).must_equal "Could not upvote"
+      must_redirect_to work_path(existing_work.id)
+      must_respond_with :redirect
     end
   end
 end
