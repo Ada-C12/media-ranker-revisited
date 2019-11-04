6c0e643463ef6b38a9c18f861394f2a07eafb84e
Eve Le <nguyenthao.lee@gmail.com>
2019-11-03 22:56:36 -0800

    Add tests for login with github action for UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "works#root"
 
-  resources :works
-  post "/works/:id/upvote", to: "works#upvote", as: "upvote"
-
-  resources :users, only: [:index, :show]
-
   get "/auth/github", as: "github_login"
-  get "/auth/:provider/callback", to: "users#create"
-
+  get "/auth/:provider/callback", to: "users#create", as: "auth_callback"
+  
+  post "/works/:id/upvote", to: "works#upvote", as: "upvote"
   delete "/logout", to: "users#destroy", as: "logout"
+
+  
+  resources :works
+  resources :users, only: [:index, :show]
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,41 @@
 require "test_helper"
 
 describe UsersController do
+    describe "create -- log in with github" do 
+        it "can log in returning user and redirects to root path" do
+            start_count = User.count
+            returning_user = users(:dan)
+            perform_login(returning_user)
+            
+            must_redirect_to root_path
+            session[:user_id].must_equal returning_user.id
+            User.count.must_equal start_count
+        end
+
+        it "can log in new user and redirects to root path" do 
+            start_count = User.count
+            new_user = User.new(
+                provider: "github", 
+                uid: 99999, 
+                name: "new user", 
+                username: "new_user", 
+                email: "test@user.com"
+            )
+            
+            perform_login(new_user)
+            
+            must_redirect_to root_path
+            session[:user_id].must_equal User.last.id
+            User.count.must_equal start_count + 1
+        end
+
+        it "redirects to root path for invalid callbacks" do
+            start_count = User.count
+            perform_login(User.new)
+            
+            must_redirect_to root_path
+            assert_nil(session[:user_id])
+            User.count.must_equal start_count
+        end
+    end
 end
