708497a16c4334ff9a7b3e96bf963b627173abf0
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-11-03 16:28:12 -0800

    added omniauth

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,23 @@
 require "test_helper"
 
 describe UsersController do
+  let(:dan) {users(:dan)}
+  describe "Login" do
+
+  #start_count = User.count
+
+  user = users(:dan)
+
+  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+  get auth_callback_path(:github)
+
+  must_redirect_to root_path
+  expect(flash[:success]).must_equal "Logged in as returning user #{user.name}"
+
+  expect(session[:user_id]).must_equal user.id
+
+ # assert(User.count == start_count_before)
+  end
 end
+# Read about fixtures at http://api.rubyonrails.org/classes/ActiveRecord/FixtureSet.html
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require "rails/test_help"
 require "minitest/rails"
 require "minitest/skip_dsl"
 require "minitest/reporters"  # for Colorized output
+require "pry"
 
 #  For colorful output!
 Minitest::Reporters.use!(
@@ -23,4 +24,31 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
+
+  def setup
+    # Once you have enabled test mode, all requests
+    # to OmniAuth will be short circuited to use the mock authentication hash.
+    # A request to /auth/provider will redirect immediately to /auth/provider/callback.
+    OmniAuth.config.test_mode = true
+  end
+
+  def mock_auth_hash(user)
+    return{
+             provider: user.provider,
+             uid: user.uid,
+             info: {
+               email: user.email,
+               name: user.name
+             }
+           }
+  end
+
+  def perform_login(user = nil)
+    user ||= User.first
+
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    get auth_callback_path(:github)
+    # session[:user_id] = "HAHAHA"
+    return user
+  end
 end
