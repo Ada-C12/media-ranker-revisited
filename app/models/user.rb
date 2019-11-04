4c13cd8b8eb3237afae9199989e6ac3def57fd6a
Mira <calopter@gmail.com>
2019-11-03 19:01:42 -0800

    update validation to match tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,7 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
-  validates :username, presence: true
-  validates :uid, presence: true
-  validates :provider, presence: true
+  validates :username, presence: true, uniqueness: true
   
   def self.build_from_github(auth_hash)
     user = User.new
