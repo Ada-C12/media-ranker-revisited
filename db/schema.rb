67ab3bb1f4e09b84ec991a191cd6518861781187
gracexinran <gracexinran@gmail.com>
2019-11-05 00:01:09 -0800

    Added user_id column to works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_051104) do
+ActiveRecord::Schema.define(version: 2019_11_05_072616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_10_22_051104) do
     t.datetime "updated_at", null: false
     t.integer "vote_count", default: 0
     t.integer "publication_year"
+    t.integer "user_id"
   end
 
   add_foreign_key "votes", "users"
