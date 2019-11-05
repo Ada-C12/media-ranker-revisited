dce52c803b097dc4444221d391abb0aae0a29c02
gracexinran <gracexinran@gmail.com>
2019-11-04 16:38:58 -0800

    Modified users and works fixtures and vote_test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :name, uniqueness: true, presence: true
-
+  validates :uid, uniqueness: true, presence: true 
+  validates :email, uniqueness: true, presence: true, format: { with: /@/, message: "format must be valid." }
+  
   def self.build_from_github(auth_hash)
     user = User.new
     user.uid = auth_hash[:uid]
