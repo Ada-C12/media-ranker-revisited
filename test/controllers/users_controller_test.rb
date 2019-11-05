cf7a755d7691378c29f86a7b015bbd41d2d0dc79
geli-gel <angele.zam@gmail.com>
2019-11-04 16:29:51 -0800

    added it-do's for login and logout

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,23 @@
 require "test_helper"
 
 describe UsersController do
+  describe "create" do
+    it "creates an account for a new user and redirects to root, storing logged in user's id in session" do
+    end
+
+    it "logs in an existing user and redirects to root, storing logged in user's id in session" do
+    end
+
+    it "does not create an account if login is performed with invalid User data, leaving session unchanged" do
+    end
+  end
+
+  describe "logout" do
+    it "logs out the user in session by setting session[:user_id] to nil" do
+    end
+
+    # maybe add this test?
+    # it "redirects to root and sends :bad_request response if no user was logged in" do
+    # end
+  end
 end
