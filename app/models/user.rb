7c46654aea7b462c56f960ed0f3627b5b8ee0be4
Brianna Kemp <kemp.bri@gmail.com>
2019-11-04 21:59:25 -0800

    Updated validation and username vs name in model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
   
+  validates :name, uniqueness: true, presence: true
+  
   def self.build_from_github(auth_hash)
     user = User.new
     user.uid = auth_hash[:uid]
