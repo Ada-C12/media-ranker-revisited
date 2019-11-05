5c3d6b1a760f23eef8c3315a8c5cf7410f858971
daniela sanchez <dnsanche@gmail.com>
2019-11-04 21:09:52 -0800

    Model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
   validates :uid, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true  
 
   def self.build_from_github(auth_hash)
     user = User.new
