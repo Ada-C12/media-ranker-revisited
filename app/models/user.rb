04a6b78aab142f556506b3ecd778b215bff295f8
geli-gel <angele.zam@gmail.com>
2019-11-04 18:19:41 -0800

    set username to come from github nickname, and made users destroyable by having their associated votes destroyed as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :destroy
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
@@ -10,6 +10,7 @@ class User < ApplicationRecord
     user.provider = "github"
     user.name = auth_hash[:info][:name]
     user.email = auth_hash[:info][:email]
+    user.username = auth_hash[:info][:nickname]
     return user
   end
 end
